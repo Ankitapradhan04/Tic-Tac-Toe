15fcac0bf63c4b8ccb8da729f8bdaf5719a47a08
Ankita Pradhan <67707036+Ankitapradhan04@users.noreply.github.com>
2021-10-13 09:13:31 +0000

    Added a new function isWin to the code.

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ def printBoard(board):
 #take user input and replace the appropriate space
 def makeMove():
     global player
-    x = int(input(player + ", What is the X coordinate? "))
+    x = int(input("player " + player + ", What is the X coordinate? "))
     y = int(input("What is the Y coordinate? "))
+    
+    while board[y][x] != " ":
+        print("you must choose an empty spot")
+        x = int(input("player " + player + ", What is the X coordinate? "))
+        y = int(input("What is the Y coordinate? "))
+    
     
     if player == "X":
         board[y][x] = "X"
@@ -19,7 +25,25 @@ def makeMove():
     else:
         board[y][x] = "O"
         palayer = "X"
-        
+
+
+def isWin():
+    global player
+    #3 in a row
+    for x in range(len(board)):
+        win = True
+        for y in range(len(board)):
+            if board[x][y] != player:
+                win = False
+                break
+        if win:
+            True
+    #3 in a coloumn
+    
+    #diagonals
+    
+    return False
+
 
 def tictac():
     printBoard(board)
